7ff56ddd14c484db69e9a7cde33a73f53995e240
Teodor Pripoae <teodor.pripoae@gmail.com>
2014-01-23 19:27:58 +0200

    Change url in init.rb

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :read_only_field do
   author 'Teodor Pripoae'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'http://github.com/teodor-pripoae/read_only_field'
   author_url 'http://toni.uige.it/about'
 end
